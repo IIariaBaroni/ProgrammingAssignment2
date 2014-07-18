53c98a7e0fdbefb7db6d53de8d81723670e14957
unknown <i.baroni@PC-BARONI.telbios.it>
2014-07-18 14:28:38 +0200

    Program assignment 2 for the R programming course completed and committed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function to take and cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    ##Internal variable for makeCacheMatrix
+    inv <- NULL
+    
+    ## set and get funcion for the x matrix
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    
+    ## set and get funcion for the inverse of the x matrix
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    
+    ##Output: a list of the functions availables
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## This function return a matrix that is the inverse of 'x'
+  ## The x variable is a makeCacheMatrix object, it's not a matrix!
+  
+  
+  ## Taking a the possible inverse matrix with the getinverse method
+  inv <- x$getinverse()
+  
+  ##If the inv matrix is not empty, we already have the result in inv, so the system return inv variable
+  if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+  }
+  ## If the matrix is not empty, the function go on, taking the original matrix from x
+  data <- x$get()
+  
+  ## Creation of the inv variable
+  inv <- solve(data)
+  
+  ## Call to the Setinverse varable in x
+  x$setinverse(inv)
+  
+  ## Output of the funcion: inv variable
+  inv
 }
